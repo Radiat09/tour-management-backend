74296709714dedf7cc6d1045ca25a847e0a2a6d5
Radiat09 <rzsianboyridoy10@gmail.com>
2025-07-12 18:50:53 +0600

    create update user api and password Re-hashing, Add custom property to  requrest parameter of express, req.user

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { UserServices } from "./user.service";
@@ -28,7 +27,26 @@ const getAllUsers = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const updateUser = catchAsync(async (req: Request, res: Response) => {
+  const userId = req.params.id;
+  // const token = req.headers.authorization
+  // const verifiedToken = verifyToken(token as string, envVars.JWT_ACCESS_SECRET) as JwtPayload
+
+  const verifiedToken = req.user;
+
+  const payload = req.body;
+  const user = await UserServices.updateUser(userId, payload, verifiedToken);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.CREATED,
+    message: "User Updated Successfully",
+    data: user,
+  });
+});
+
 export const UserControllers = {
   createUser,
   getAllUsers,
+  updateUser,
 };
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { UserControllers } from "./user.controller";
 import { validateRequest } from "../../middlewares/validateRequest";
-import { createUserZodSchema } from "./user.validation";
+import { createUserZodSchema, updateUserZodSchema } from "./user.validation";
 import { Role } from "./user.interface";
 import { checkAuth } from "../../middlewares/checkAuth";
 
@@ -16,6 +16,12 @@ router.get(
   "/all-users",
   checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
   UserControllers.getAllUsers
+);
+router.patch(
+  "/:id",
+  validateRequest(updateUserZodSchema),
+  checkAuth(...Object.values(Role)),
+  UserControllers.updateUser
 );
 
 export const UserRoutes = router;
